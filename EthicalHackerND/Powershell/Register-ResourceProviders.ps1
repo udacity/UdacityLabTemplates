753932c168a13bbd751493de9f043f296d2181b9
Sudhanshu Kulshrestha <48475411+SudKul@users.noreply.github.com>
2022-11-17 16:55:04 +0530

    Update Register-ResourceProviders.ps1

--- EthicalHackerND/Powershell/Register-ResourceProviders.ps1
+++ EthicalHackerND/Powershell/Register-ResourceProviders.ps1
@@ -1,12 +1,14 @@
-#PowerShell script to register resource providers
+# PowerShell script to register resource providers
 
-#Install's and import's the Az Module
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Set-PSRepository -Name "PSGallery" -Installationpolicy Trusted
-Install-Module -Name Az -AllowClobber -Scope AllUsers -Force
-Import-Module -Name Az
+# Needed only if running the current file from a local Powershell terminal
+# Install's and import's the Az Module
+# Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
+# Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+# Set-PSRepository -Name "PSGallery" -Installationpolicy Trusted
+# Install-Module -Name Az -AllowClobber -Scope AllUsers -Force
+# Import-Module -Name Az
 
-Connect-AzAccount
+# Connect-AzAccount
 
 # ToDo for the Student: Enter the subscription ID you want to use
 Select-AzSubscription -SubscriptionId "xxxxxxxx-xxxxxxxx-xxxxxxxx-xxxxxxxx"
